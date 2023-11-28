0ddc3f181f99ccccf55e70d9cebbd619d6ea0a69
polux-jney <gabord4532@gmail.com>
2023-11-28 17:17:17 -0600

    Server HTTP Ubicacion cambiada

--- index.php
+++ index.php
@@ -1,12 +1,5 @@
 <?php
-if ($_SERVER['HTTPS'] != "on") { 
-  $url = "https://". $_SERVER['SERVER_NAME'] . $_SERVER['REQUEST_URI']; 
-  header("Location: $url"); 
-  exit; 
-}
-
 require 'view/header.php';
-
 ?>
   <!--========================================
        ============SLIDER DE IMG===================
