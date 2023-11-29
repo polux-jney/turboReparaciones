d37cb2dfa75740fef3a3b107f32bdf2b6317330a
polux-jney <79025870+polux-jney@users.noreply.github.com>
2023-11-28 19:14:59 -0600

    Update contacto.php Next

--- view/contacto.php
+++ view/contacto.php
@@ -40,7 +40,7 @@ require 'header.php';
         </div>
 
         <input type="hidden" name="_template" value="table">
-        <input type="hidden" name="_next" value=" ">
+        <input type="hidden" name="_next" value="https://turboreparaciones-production.up.railway.app">
         <div class="mb-3">
           <button type="submit" class="btn btn-primary w-100 fs-5">Enviar mensaje</button>
         </div>
